25e4bb04dd886eb413a45173d2b12630998b6695
operagang <woojin.s0106@gmail.com>
2025-12-25 19:42:27 +0900

    .

---
+++ init_heuristics.py
@@ -0,0 +1,90 @@
+import random
+
+def sgs_a(instance):
+
+    T = instance['T']
+    a = instance['a']
+    b = instance['b']
+    Xi = instance['Xi']
+    V = instance['V']
+    l0 = instance['l^0']
+    Vt = instance['V_tau']
+    ls = instance['l^1']
+    lt = instance['l^2']
+    that = instance['hat(t)']
+    Theta = instance['Theta']
+    h = instance['h']
+    Delta = instance['Delta']
+    g = instance['g']
+    Tobj = instance['T^obj']
+    r = instance['r']
+    crane_tr = instance['crane_tr']
+
+    S, ES, W, Q, G, C, L = {}, {}, {}, {}, {}, {}, {}
+
+    for t in T:
+        ES[t] = a[t]
+        W[t] = set(t2 for t2 in T if (t, t2) in Xi)
+        Q[t] = len(set(t2 for t2 in T if (t2, t) in Xi))
+    
+    for v in V:
+        G[v] = set()
+        C[v] = 0
+        L[v] = l0[v]
+
+    U = set(T)
+
+    while U:
+        A = set(t for t in U if Q[t] == 0)
+        tstar = min(A, key=lambda t: ES[t])
+
+
+        min_val = min(ES[t] for t in A)
+        tstars = [t for t in A if ES[t] == min_val]
+        if len(tstars) > 1:
+            pass
+        tstar = random.choice(tstars)
+
+
+        Vprime = set()
+        E = dict()
+
+        for v in Vt[tstar]:
+            est = max(ES[tstar], C[v] + abs(ls[tstar] - L[v]) * that)
+            for vprime in V:
+                if crane_tr[vprime] != crane_tr[v]:
+                    continue
+                if vprime == v:
+                    continue
+                for tprime in G[vprime]:
+                    if (tprime, tstar, vprime, v) in Theta:
+                        est = max(est, S[tprime] + h[tprime] + Delta[(tprime, tstar, vprime, v)])
+                
+                Vprime.add(v)
+                E[v] = est
+
+        vstar = min(Vprime, key=lambda v: E[v])
+
+
+        min_val = min(E[v] for v in Vprime)
+        vstars = [v for v in Vprime if E[v] == min_val]
+        if len(vstars) > 1:
+            pass
+        vstar = random.choice(vstars)
+
+
+        S[tstar] = E[vstar]
+        G[vstar].add(tstar)
+        C[vstar] = E[vstar] + h[tstar]
+        L[vstar] = lt[tstar]
+        U.remove(tstar)
+
+        for t in W[tstar]:
+            Q[t] -= 1
+            ES[t] = max(ES[t], S[tstar] + g[tstar,t])
+
+    obj = sum(S[t] + h[t] - r[t] for t in Tobj)
+
+    delay = sum(max(0, S[t] - b[t]) for t in T)
+
+    return S, G, obj, delay
---
+++ main.py
@@ -0,0 +1,53 @@
+import time
+from tqdm import tqdm
+from utils import load_instance
+from init_heuristics import sgs_a
+
+
+
+
+def main(n_cranes, n_jobs, instance_idx, n_sampling=1):
+    # 인스턴스 경로 생성
+    inst_path = f"./instances/{n_cranes}_{n_jobs}_{instance_idx}.json"
+    # 로딩
+    instance = load_instance(inst_path)
+
+    results = []
+    for _ in tqdm(range(n_sampling)):
+    # for _ in range(n_sampling):
+        # 휴리스틱 실행
+        start = time.time()
+        schedule, assignment, obj, delay = sgs_a(instance)
+        runtime = time.time() - start
+
+        # 결과 출력
+        # print(f"[INFO] Instance: C={n_cranes}, J={n_jobs}, idx={instance_idx}")
+        # print(f"obj={obj}, delay={delay}, runtime={runtime:.4f} sec")
+        # print()
+
+        results.append((obj, delay))
+    results.sort(key=lambda x: (x[1], x[0]))
+    print(instance_idx, results[0])
+
+
+if __name__ == "__main__":
+    # for n_cranes in [2, 3]:
+    #     if n_cranes == 2 :
+    #         job_list = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90, 100]
+    #     else :
+    #         job_list = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+    #     for n_jobs in job_list :
+    #         for instance_idx in range(30):
+    #             main(
+    #                 n_cranes=n_cranes,
+    #                 n_jobs=n_jobs,
+    #                 instance_idx=instance_idx,
+    #             )
+    
+    for idx in range(30):
+        main(
+            n_cranes=2,
+            n_jobs=5,
+            instance_idx=idx,
+            n_sampling=100
+        )
